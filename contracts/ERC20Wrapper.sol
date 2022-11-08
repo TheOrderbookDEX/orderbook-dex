50ee6f84600dda2d5546fa4a65b8d753e2be1047
Frugal-Wizard <108354301+Frugal-Wizard@users.noreply.github.com>
2022-11-08 20:34:41 -0300

    update solc to 0.8.17

--- contracts/ERC20Wrapper.sol
+++ contracts/ERC20Wrapper.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: BUSL-1.1
 
-pragma solidity 0.8.15;
+pragma solidity 0.8.17;
 
 /**
  * An ERC20 token wrapper that allows non standard tokens to be used in an orderbook.
