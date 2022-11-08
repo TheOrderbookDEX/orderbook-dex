a0100e990887f6a38a59e3f134553875d9a2ff31
Frugal-Wizard <108354301+Frugal-Wizard@users.noreply.github.com>
2022-11-08 11:32:19 -0300

    add price observers to IOrderbook
    also changed token observers return type to address instead IERC20

--- contracts/interfaces/IOrderbook.sol
+++ contracts/interfaces/IOrderbook.sol
@@ -2,8 +2,6 @@
 
 pragma solidity ^0.8.0;
 
-import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-
 /**
  * Orderbook exchange for a token pair.
  */
@@ -14,37 +12,67 @@ interface IOrderbook {
      * From right to left, the first two digits is the patch version, the second two digits the minor version,
      * and the rest is the major version, for example the value 10203 corresponds to version 1.2.3.
      *
-     * @return the orderbook version
+     * @return version the orderbook version
      */
-    function version() external view returns (uint32);
+    function version() external view returns (uint32 version);
 
     /**
      * The token being traded.
      *
-     * @return  the token being traded
+     * @return tradedToken the token being traded
      */
-    function tradedToken() external view returns (IERC20);
+    function tradedToken() external view returns (address tradedToken);
 
     /**
      * The token given in exchange and used for pricing.
      *
-     * @return  the token given in exchange and used for pricing
+     * @return baseToken the token given in exchange and used for pricing
      */
-    function baseToken() external view returns (IERC20);
+    function baseToken() external view returns (address baseToken);
 
     /**
      * The size of a contract in tradedToken.
      *
-     * @return  the size of a contract in tradedToken
+     * @return contractSize the size of a contract in tradedToken
      */
-    function contractSize() external view returns (uint256);
+    function contractSize() external view returns (uint256 contractSize);
 
     /**
      * The price tick in baseToken.
      *
      * All prices are multiples of this value.
      *
-     * @return  the price tick in baseToken
+     * @return priceTick the price tick in baseToken
      */
-    function priceTick() external view returns (uint256);
+    function priceTick() external view returns (uint256 priceTick);
+
+    /**
+     * The ask price in baseToken.
+     *
+     * @return askPrice the ask price in baseToken
+     */
+    function askPrice() external view returns (uint256 askPrice);
+
+    /**
+     * The bid price in baseToken.
+     *
+     * @return bidPrice the bid price in baseToken
+     */
+    function bidPrice() external view returns (uint256 bidPrice);
+
+    /**
+     * The next available sell price point.
+     *
+     * @param  price         an available sell price point
+     * @return nextSellPrice the next available sell price point
+     */
+    function nextSellPrice(uint256 price) external view returns (uint256 nextSellPrice);
+
+    /**
+     * The next available buy price point.
+     *
+     * @param  price        an available buy price point
+     * @return nextBuyPrice the next available buy price point
+     */
+    function nextBuyPrice(uint256 price) external view returns (uint256 nextBuyPrice);
 }
