ca4e56ac125da8bf2e4703634a52bb2bf39781a4
Frugal-Wizard <108354301+Frugal-Wizard@users.noreply.github.com>
2023-05-19 14:02:39 -0300

    update deps

--- contracts/testing/ERC20WithFaucet.sol
+++ contracts/testing/ERC20WithFaucet.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: BUSL-1.1
 
-pragma solidity 0.8.17;
+pragma solidity 0.8.20;
 
 import { IERC20Metadata } from "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
 import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
